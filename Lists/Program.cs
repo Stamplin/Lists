ea163ea38625bd38e91c871613a55d18b626c39c
Stamplin <701017313@gotvdsb.ca>
2024-10-03 09:36:14 -0400

    Update Program.cs

--- Lists/Program.cs
+++ Lists/Program.cs
@@ -25,28 +25,48 @@ Console.WriteLine("The random color selected for you is:");
 Console.WriteLine();
 Console.WriteLine(colors[randnum]);
 Console.ReadLine();
-
+Console.Clear();
 
 //task 2 - Random Numbers
 int min, max, num, gen;
-List<int> randomNumbers = new List<int>();
+max = 0;
+
+
 Console.WriteLine("How many numbers do you need?");
 Int32.TryParse(Console.ReadLine(), out num);
+
 Console.WriteLine("Enter a maximum value:");
-if (max > num)
-{
-    Console.WriteLine("Your max is higher than the number you picked.");
-}
 Int32.TryParse(Console.ReadLine(), out max);
+
 Console.WriteLine("Enter a minimum value:");
 Int32.TryParse(Console.ReadLine(), out min);
 
-
-gen = generator.Next(min, max);
-
-for (int i = min; i < max; i++) 
+List<int> randomNumbers = new List<int>();
+for (int i = 0; i < num; i++)
 {
-    randomNumbers.Add(gen);
+    randomNumbers.Add(generator.Next(min, max + 1));
+}
+//1 - show the list
+Console.WriteLine("List of random numbers:");
+Console.WriteLine(string.Join(", ", randomNumbers));
+//2 - count how many is on the list
+Console.WriteLine("Enter a number to count its appears in the list:");
+int countNum = int.Parse(Console.ReadLine());
+int count = 0;
+foreach (int number in randomNumbers) //ik we didn't learn this but I watched a youtube toturial
+{
+    if (number == countNum) count++;
+}
+Console.WriteLine($"The number {countNum} appears {count} times in the list.");
+//3 - replace w zero
+for (int i = 0; i < randomNumbers.Count; i++)
+{
+    if (randomNumbers[i] == countNum)
+        randomNumbers[i] = 0; //count how many of the num is in the list and replace them witho zero
 }
 
-Console.WriteLine(randomNumbers.ToArray());
+
+
+
+
+Console.ReadLine();
